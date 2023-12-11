1909df7c7cdced64acab7372f74240348bc0f683
Alberto del Real <66703149+del-Real@users.noreply.github.com>
2023-12-11 12:04:18 +0100

    sidebar menu added

---
+++ Window1.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FisioLogic
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void mainFrame_Navigated_1(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new Uri("pages/Citas.xaml", UriKind.Relative));
+
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new Uri("pages/Pacientes.xaml", UriKind.Relative));
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new Uri("pages/Personal.xaml", UriKind.Relative));
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new Uri("pages/Historiales.xaml", UriKind.Relative));
+        }
+    }
+}
+
